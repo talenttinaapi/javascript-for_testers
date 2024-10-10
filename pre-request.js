2d363958fd46a956e64973b79fc3695ec6da528e
talent tinaapi <ttinaapi88@gmail.com>
2024-10-10 15:28:49 +0200

    reqs

---
+++ pre-request.js
@@ -0,0 +1,5 @@
+// Set an environment variable without any initial value 
+pm.environment.set("eighthProduct");
+
+// Set an environment variable with an initial value 
+pm.environment.set("PI", 3.141);
